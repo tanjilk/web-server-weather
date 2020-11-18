544ced1a7c97f8a0d44929c8724a67cbcf229884
tanjilk <tanjilkh@gmail.com>
2020-11-18 18:02:57 +0000

    static files

--- src/app.js
+++ src/app.js
@@ -1,17 +1,47 @@
+const path = require('path');
+
 const express = require('express');
+
 const app = express()
 
+console.log(__dirname);
+// console.log(__filename);
+
+console.log(path.join(__dirname, '../public'));
+
+const publicDirectory = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectory));
+
+/*
 app.get('', (req, res) => {
     res.send('<h1>Hello Express</h1>');
 })
+*/
 
 app.get('/weather', (req, res) => {
-    res.send('<h1>Your weather</h1');
+    res.send({
+        forecast: 'No raining! Nice!',
+        location: 'Porto',
+    })
 })
 
-app.get('/about', (req, res) => {
-    res.send("<h1>About</h1>");
-})
+app.get('/people', (req, res) => {
+    res.send([
+        {
+            name: 'Tanjil',
+            age: 15
+        },
+        {
+            name: 'John',
+            age: 20
+        },
+        {
+            name: 'Jeff',
+            age: 19,
+        },
+    ])
+});
 
 app.listen(3000, () => {
     console.log('Running the app in 3000.');
