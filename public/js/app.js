12c5490a390ea306bb615666297b5bbd090ed80a
tanjilk <tanjilkh@gmail.com>
2020-12-19 12:16:41 +0000

    get request with fetch

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,17 @@
 console.log('Client side javascript is loeaded!');
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data);
+  })
+})
+
+
+fetch('http://localhost:3000/weather?search=porto').then((response) => {
+    response.json().then((data, error) => {
+        if(error){
+            console.log(error)
+        }
+        console.log(data);
+    })
+})
